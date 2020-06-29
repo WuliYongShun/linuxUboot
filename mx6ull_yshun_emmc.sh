1f44cd47b2870d26eafc41064746a810df44dcee
WuliYongShun <471972509@qq.com>
2020-06-29 23:01:35 +0800

    first commot

---
+++ mx6ull_yshun_emmc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- mx6ull_yshun_emmc_defconfig
+make V=1 ARCH=arm CROSS_COMPILE=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- -j8
